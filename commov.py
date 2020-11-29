aa7ec6d21d8e48eef4b6e7901a3018ebdb005978
Antônio Ferreira <antonio.carlos.o.ferreira@gmail.com>
2020-11-28 22:16:36 -0300

    Calculating link budget, max radius and max throughput.

--- commov.py
+++ commov.py
@@ -1,4 +1,6 @@
-from math import sqrt
+from math import sqrt, log10, pow
+from matplotlib import pyplot as plt
+
 import argparse
 
 def count_hex(area, radius, reuse):
@@ -10,6 +12,25 @@ def count_hex(area, radius, reuse):
 
     return total_hex, total_clusters
 
+def link_budget(pot_tx, pot_rx, snr, Md, Gt, Pt):
+    Lmax = pot_tx - pot_rx + Gt - Pt - snr - Md
+    return Lmax
+
+def max_radius(max_loss, freq, hb, hm):
+    x = 44.9 - 6.55*hb
+    a = (1.11*log10(freq) - 0.7)*hm - 1.56*log10(freq) - 0.8
+    log_R_max = (max_loss - 69.55 - 26.16*log10(freq) + 13.82*log10(hb) + a)/x
+    R_max = pow(10, log_R_max)
+    
+    return R_max
+
+def max_throughput(bw, bpsimb):
+    t_simb = 0.00001
+    subcarries = bw*60
+    throughput = t_simb*subcarries*bpsimb
+
+    return throughput
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Input arguments")
     parser.add_argument('--bandwidth', '-b', 
@@ -20,3 +41,5 @@ if __name__ == "__main__":
                         type=int,
                         help="Valid frequency reuse factor. Default = 7",
                         default=7)
+    
+    inputs = parser.parse_args()
